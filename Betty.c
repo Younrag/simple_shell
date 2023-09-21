c435420776a303d50e2643e1c63be36803e05cd6
Younrag <younes.raguebi@usmba.ac.ma>
2023-09-21 02:08:11 +0000

    Updated

---
+++ Betty.c
@@ -0,0 +1,18 @@
+/**
+ * main - Entry point of the program
+ *
+ * Description: This is a simple program written in accordance
+ *              with the Betty style guide.
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("Hello, Betty!\n");
+	}
+
+	return (0);
+}
